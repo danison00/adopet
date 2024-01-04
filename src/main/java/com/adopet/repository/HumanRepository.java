60a02b8d3e5f3159904f3919ad82481c514809e1
Danison dos Santos <danison.santos00@gmail.com>
2024-01-04 20:34:32 -0300

    add: human rep

--- src/main/java/com/adopet/repository/HumanRepository.java
+++ src/main/java/com/adopet/repository/HumanRepository.java
@@ -4,5 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.adopet.model.entity.Human;
 
 public interface HumanRepository  extends JpaRepository<Human, Long>{
+
+    void deleteByUsername(String username);
+
+    Human findByUsername(String username);
     
 }
