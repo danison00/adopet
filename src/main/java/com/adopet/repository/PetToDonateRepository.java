98de99f02605fa2035450feb8ba3e782e581ba14
Danison dos Santos <danison.santos00@gmail.com>
2024-01-04 21:13:11 -0300

    fix: add findByHuman

--- src/main/java/com/adopet/repository/PetToDonateRepository.java
+++ src/main/java/com/adopet/repository/PetToDonateRepository.java
@@ -1,8 +1,14 @@
 package com.adopet.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.adopet.model.entity.Human;
 import com.adopet.model.entity.PetToDonate;
 
 public interface PetToDonateRepository extends JpaRepository<PetToDonate, Long> {
+
+    List<PetToDonate> findByHuman(Human human);
     
 }
