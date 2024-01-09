69f354be7a3fe9749a3fc8c89ee8bdff534e5a5e
Danison dos Santos <danison.santos00@gmail.com>
2024-01-08 21:53:23 -0300

    up: add findBycpf

--- src/main/java/com/adopet/repository/HumanRepository.java
+++ src/main/java/com/adopet/repository/HumanRepository.java
@@ -1,12 +1,15 @@
 package com.adopet.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.adopet.model.entity.Human;
 
 public interface HumanRepository extends JpaRepository<Human, Long> {
 
     boolean existsByCpf(String cpf);
+    boolean existsByEmail(String email);
 
-    Human findByCpf(String cpf);
+    Optional<Human> findByCpf(String cpf);
 
 }
