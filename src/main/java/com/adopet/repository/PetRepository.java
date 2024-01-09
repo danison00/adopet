f0d3c88ba316e6ceb5446791eb22e96cc0c46d47
Danison dos Santos <danison.santos00@gmail.com>
2024-01-09 20:06:19 -0300

    add: findbyOwner

--- src/main/java/com/adopet/repository/PetRepository.java
+++ src/main/java/com/adopet/repository/PetRepository.java
@@ -1,10 +1,13 @@
 package com.adopet.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.adopet.model.entity.Pet;
 
 public interface PetRepository extends JpaRepository<Pet, Long> {
 
+    List<Pet> findByOwnerId(Long id);
     
 }
